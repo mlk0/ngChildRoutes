291d651fed877a1117a4d15aca0de39b39f35666
m1k0 <acme@gmx.com>
2018-02-08 00:06:14 -0500

    added children to the product route

--- ngChildRoutes/src/app/app.module.ts
+++ ngChildRoutes/src/app/app.module.ts
@@ -5,22 +5,37 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { BumpsComponent } from './bumps/bumps.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCategoriesComponent } from './product-categories/product-categories.component';
+import { ProductInventoryComponent } from './product-inventory/product-inventory.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    BumpsComponent
+    BumpsComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductCategoriesComponent,
+    ProductInventoryComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
 
       [
-        { path : '', pathMatch : 'full', redirectTo : 'welcome'},
-        { path : 'welcome', component : WelcomeComponent },
-        { path : 'bum', component : BumpsComponent}
+        { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+        { path: 'welcome', component: WelcomeComponent },
+        {
+          path: 'products', component: ProductsComponent, children: [
+            { path: 'list', component: ProductListComponent },
+            { path: 'categories', component: ProductCategoriesComponent },
+            { path: 'inventory', component : ProductInventoryComponent}
+          ]
+        }
+
       ]
     )
   ],
