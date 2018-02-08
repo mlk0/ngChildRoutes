d70dc98a22da555f03f29bbe651cde2c14a5caf8
m1k0 <acme@gmx.com>
2018-02-07 22:38:01 -0500

    welcome component set to be target to the default route

--- ngChildRoutes/src/app/app.module.ts
+++ ngChildRoutes/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+
+      [
+        { path : '', pathMatch : 'full', redirectTo : 'welcome'},
+        { path : 'welcome', component : WelcomeComponent }]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
