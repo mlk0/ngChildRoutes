3ed959e6e7fe65080ab0e846c3f06966a99955bc
m1k0 <acme@gmx.com>
2018-02-10 04:30:57 -0500

    moving the product components in the Product module directory

--- ngChildRoutes/src/app/product/product.module.ts
+++ ngChildRoutes/src/app/product/product.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from '../products/products.component';
-import { ProductListComponent } from '../product-list/product-list.component';
-import { ProductCategoriesComponent } from '../product-categories/product-categories.component';
-import { ProductInventoryComponent } from '../product-inventory/product-inventory.component';
+
 import { RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCategoriesComponent } from './product-categories/product-categories.component';
+import { ProductInventoryComponent } from './product-inventory/product-inventory.component';
 
 @NgModule({
   imports: [
